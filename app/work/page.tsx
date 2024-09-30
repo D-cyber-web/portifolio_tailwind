7abc982871ee08c83ab5d45ddb30aa1f3943ea67
Davie <muthama3david@gmail.com>
2024-09-30 09:54:56 +0300

    updates my-portifolio

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -18,13 +18,51 @@ const projects = [
         category: 'frontend',
         title: 'project 1',
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur delectus ut eligendi labore explicabo eaque.",
+        stack: [{ name: "Html 5"}, { name: "Css 3"}, { name:"Laravel" }],
+        image: "/assets/work/hehe1.png",
+        live: "",
+        github: "",
+    },
+    {
+        num: '02',
+        category: 'fullstack',
+        title: 'project 2',
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur delectus ut eligendi labore explicabo eaque.",
+        stack: [{ name: "Next.js"}, { name: "Tailwind.css"}, { name:"Node.js" }],
+        image: "/assets/work/hehe2.png",
+        live: "",
+        github: "",
+    },
+    {
+        num: '01',
+        category: 'frontend',
+        title: 'project 3',
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur delectus ut eligendi labore explicabo eaque.",
+        stack: [{ name: "Html 5"}, { name: "Css 3"}, { name:"Javascript" }],
+        image: "/assets/work/hehe3.png",
+        live: "",
+        github: "",
+    },
+    {
+        num: '01',
+        category: 'frontend',
+        title: 'project 4',
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur delectus ut eligendi labore explicabo eaque.",
         stack: [{ name: "Html 5"}, { name: "Css 3"}],
+        image: "/assets/work/hehe4.png",
+        live: "",
+        github: "",
     },
 ];
 
  const Work = () => {
+    const [project, setProject] = useState(projects[0]);
     return (
-        <div>Work page</div>
+        <motion.section initial={{opacity: 0}}>
+            <div className="container mx-auto">
+                project
+            </div>
+        </motion.section>
     )
  }
 
