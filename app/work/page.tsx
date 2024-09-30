a9461636a6a1ef4e1d775e8fa0753829187c749e
Davie <muthama3david@gmail.com>
2024-09-30 09:09:32 +0300

    updates my-portifolio

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,6 +1,26 @@
 "use client";
 
-import {} from 'framer-motion'
+import {motion} from 'framer-motion'
+import React, {useState} from 'react';
+
+import { Swiper, SwiperSlide } from "swiper/react"
+import "swiper/css"
+
+import { BsArrowUpRight, BsGithub } from 'react-icons/bs';
+
+import { Tooltip, TooltipContent, TooltipProvider,TooltipTrigger } from "@/components/ui/tooltip";
+import Link from 'next/link';
+import Image from 'next/image';
+
+const projects = [
+    {
+        num: '01',
+        category: 'frontend',
+        title: 'project 1',
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur delectus ut eligendi labore explicabo eaque.",
+        stack: [{ name: "Html 5"}, { name: "Css 3"}],
+    },
+];
 
  const Work = () => {
     return (
